45ed8857d164cc5f313d1fd58c0cd5a2463a5dcd
NIJ18482 <60576547+NIJ18482@users.noreply.github.com>
2020-05-15 21:40:57 -0600

    configuraciones
    Configuracion de GPIO y clock

--- Nueva carpeta (2)/Experimentacion_1/main.c
+++ Nueva carpeta (2)/Experimentacion_1/main.c
@@ -7,13 +7,47 @@
 #include "inc/tm4c123gh6pm.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
+int estado = 0;
+int enable = 0;
+int contador_0 = 0;
+int contador_1 = 0;
+int contador_2 = 0;
+int contador_3 = 0;
+int contador_4 = 0;
+int contador_5 = 0;
 
 
 
 
-int main()){
-    SysCtlClockSet(SYSCTL_XTAL_16MHZ|SYSCTL_SYSDIV_10);
+
+int main(){
+    SysCtlClockSet(SYSCTL_SYSDIV_10|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4);
+    GPIOPadConfigSet(GPIO_PORTF_BASE,GPIO_PIN_4,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD_WPU);
+    GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,14);
+    while(true){
+        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,14);
+        SysCtlDelay(2000000);
+        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0);
+        SysCtlDelay(2000000);
+
+
+
+
+
+
+
+
+        /*estado = GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_4);
+        if (estado == 0){
+           GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,6);
+        }else{
+            GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0);
+        }*/
+
+    }
 
 
 }
