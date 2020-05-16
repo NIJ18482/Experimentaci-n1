379f1ee45d436312009e99e178c3d2ed7ae047e0
NIJ18482 <60576547+NIJ18482@users.noreply.github.com>
2020-05-15 20:08:33 -0600

    adición de librerias y parámetros

--- Nueva carpeta (2)/Experimentacion_1/main.c
+++ Nueva carpeta (2)/Experimentacion_1/main.c
@@ -1,5 +1,19 @@
-/* Experimentación 1*/
-int main(void)
-{
-	return 0;
+/* Experimentacon 1*/
+
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/tm4c123gh6pm.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+
+
+
+
+int main()){
+    SysCtlClockSet(SYSCTL_XTAL_16MHZ|SYSCTL_SYSDIV_10);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+
+
 }
