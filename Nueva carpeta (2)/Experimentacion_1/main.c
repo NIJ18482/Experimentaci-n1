4acdf50be62825f4fbaeb3007a3a38354a48865a
NIJ18482 <60576547+NIJ18482@users.noreply.github.com>
2020-05-15 22:00:46 -0600

    adhesión de codigo principal
    codificación del semáforo.

--- Nueva carpeta (2)/Experimentacion_1/main.c
+++ Nueva carpeta (2)/Experimentacion_1/main.c
@@ -28,26 +28,31 @@ int main(){
     GPIOPadConfigSet(GPIO_PORTF_BASE,GPIO_PIN_4,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD_WPU);
     GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,14);
     while(true){
-        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,14);
-        SysCtlDelay(2000000);
-        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0);
-        SysCtlDelay(2000000);
-
-
-
-
-
-
-
-
-        /*estado = GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_4);
-        if (estado == 0){
-           GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,6);
-        }else{
+        estado = GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_4);
+        if (estado == 0){enable = 1;}
+        if (enable == 1){
+            GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,2);
+            SysCtlDelay(16000000);
             GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0);
-        }*/
-
-    }
-
-
+            SysCtlDelay(4000000);
+            GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,10);
+            SysCtlDelay(12000000);
+            GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0);
+            SysCtlDelay(4000000);
+            GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,8);
+            SysCtlDelay(20000000);
+            GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0);
+            SysCtlDelay(2000000);
+            GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,8);
+            SysCtlDelay(8000000);
+            GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0);
+            SysCtlDelay(2000000);
+            GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,8);
+            SysCtlDelay(8000000);
+            GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0);
+            SysCtlDelay(2000000);
+            enable = 0;
+            GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,14);
+        }
+   }
 }
